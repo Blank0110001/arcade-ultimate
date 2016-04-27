070dbe62ca278838c6b83ccb318837810fa653f3
leviu02 <leviu02@gmail.com>
2016-04-26 21:29:08 -0400

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -2,5 +2,5 @@ var c = document.getElementById("gameCanvas");
 var ctx = c.getContext("2d");
 ctx.fillStyle = "#072f85";
 
-ctx.roundRect(10, 10, 100, 100);
+ctx.rect(10, 10, 100, 100);
 ctx.fill();
