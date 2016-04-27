1af89108b9f11afec03bc54ef655a2c1310f7846
leviu02 <leviu02@gmail.com>
2016-04-27 12:11:50 -0400

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -2,54 +2,17 @@ var c = document.getElementById("gameCanvas");
 var ctx = c.getContext("2d");
 ctx.fillStyle = "#072f85";
 
-render();
+canvas.width = 400;
+canvas.height = 500;
 
-var paddleX = 150;
-var paddleY = 480;
+var paddleHeight = 20;
+var paddleWidth = 100;
+var paddleX = (canvas.width-paddleWidth)/2;
 
-ctx.rect(paddleX, paddleY, 100, 20);
-ctx.fill();
-
-var Keys = {
-        left: false,
-        right: false
-    };
-
-window.onkeydown = function(e) {
-    var kc = e.keyCode;
-    e.preventDefault();
-
-    if (kc === 37) Keys.left = true;
-    else if (kc === 39) Keys.right = true;
-};
-
-window.onkeyup = function(e) {
-    var kc = e.keyCode;
-    e.preventDefault();
-
-    if (kc === 37) Keys.left = false;
-    else if (kc === 39) Keys.right = false;
-};
-
-var isDirty = false;
-
-function update() {
-    if (Keys.left) {
-        paddleX+=3;
-        isDirty = true;
-    }
-    else if (Keys.right) {
-        paddleX-=3;
-        isDirty = true;
-    }
-
-    if (isDirty) render();
-    requestAnimationFrame(update);
-}
-requestAnimationFrame(update);
-
-function render() {
-    ctx.setTransform(1,0,0,1,paddleX,paddleY);
-    ctx.fillRect(paddleX, paddleY, 100,20);
-    isDirty= false;
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
 }
