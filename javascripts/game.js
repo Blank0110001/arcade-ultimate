b4caf1524c9926925b123d0e2c53c4f3420b11c1
leviu02 <leviu02@gmail.com>
2016-04-27 12:29:12 -0400

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -57,7 +57,14 @@ function draw() {
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
-    
+     else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+        }
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
         paddleX += 4;
     }
