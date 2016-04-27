2a9923d7a30051a27b7f6e32cbc6376a069f4add
leviu02 <leviu02@gmail.com>
2016-04-26 22:08:21 -0400

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -7,3 +7,14 @@ var paddleY = 480;
 
 ctx.rect(paddleX, paddleY, 100, 20);
 ctx.fill();
+
+document.addEventListener('keydown', function(event) {
+    //left
+    if(event.keyCode == 37) {
+        paddleX -= 1;
+    }
+    //right
+    else if(event.keyCode == 39) {
+        paddleX+= 1;
+    }
+});
